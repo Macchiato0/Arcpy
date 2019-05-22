e34ca62d0287cf024e986b8c5581a81f847b9ff1
Macchiato0 <50885101+Macchiato0@users.noreply.github.com>
2019-05-22 14:33:03 -0400

    Add files via upload

---
+++ def_move_sp_to_tlm.py
@@ -0,0 +1,30 @@
+#move the disconnected service points to the correct tlm
+def move_sp(it):
+    select_id='OBJECTID='+str(it)
+    select_tlm = arcpy.da.SearchCursor(r"Customers & Transformers\Service Point", ["TLM"], select_id)
+    for row in select_tlm:
+        tlm=str(row[0])
+    sel="TLM=" + "'"+tlm+"'"
+    select_section=arcpy.da.SearchCursor(r"Customers & Transformers\Secondary Transformers",["SHAPE@"], sel)
+    for i in select_section:
+        location = i[0]
+        pnt=location.centroid
+        coordinate=pnt.X+10,pnt.Y+10
+    update_pt = arcpy.da.UpdateCursor(r"Customers & Transformers\Service Point", ["SHAPE@"])
+    for ro in update_pt:
+        try:
+            ro[0] = coordinate
+            update_pt.updateRow(ro)
+        except:
+            pass
+    return it
+
+
+
+cursor=arcpy.da.SearchCursor(r"Service Point selection",["OID@"])
+obj=[]
+for i in cursor:
+    obj.append(i[0])
+    
+for i in obj:
+   move_sp(i) 
