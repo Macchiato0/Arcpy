55591928ee2497647a1c2f17ee962e1a806facef
Macchiato0 <50885101+Macchiato0@users.noreply.github.com>
2019-05-22 16:29:08 -0400

    Update Address_validate.py

--- Address_validate.py
+++ Address_validate.py
@@ -5,8 +5,18 @@ import arcpy
 
 cursor=arcpy.da.SearchCursor("AuditArea selection",["COMMENTS"])
 
-comment=[row[0] for row in cursor]
+row_list=[row[0] for row in cursor]
+
+#normalize and ignore some confused unicode data
+
+comment=[unicodedata.normalize('NFKD', i).encode('ascii','ignore') for i in row_list]
 
 # Street Address search in a string of comment
+
+import re
+
+# split every string in cooment into word by word 
+comment_split=[re.split("\s", i) for i in comment]
+
 
 
