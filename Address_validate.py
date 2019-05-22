2e6d6b87307fffa9c65e54f42794453f3e2e173c
Macchiato0 <50885101+Macchiato0@users.noreply.github.com>
2019-05-22 15:07:15 -0400

    Update Address_validate.py

--- Address_validate.py
+++ Address_validate.py
@@ -1,1 +1,12 @@
+# creat a layer of audit area complete-fail
+# extract the address from the comment of very entry of field audit
+# address=*+digit + two strings after the digit+*
+import arcpy
+
+cursor=arcpy.da.SearchCursor("AuditArea selection",["COMMENTS"])
+
+comment=[row[0] for row in cursor]
+
+# Street Address search in a string of comment
+
 
