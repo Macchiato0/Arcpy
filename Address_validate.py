cc56d21008377a0e3e19c7b66d727788f6e1435f
Macchiato0 <50885101+Macchiato0@users.noreply.github.com>
2019-05-23 09:52:33 -0400

    Update Address_validate.py

--- Address_validate.py
+++ Address_validate.py
@@ -8,11 +8,10 @@ cursor=arcpy.da.SearchCursor("AuditArea selection",["COMMENTS"])
 row_list=[row[0] for row in cursor]
 
 # normalize and ignore some confused unicode data
-
+import unicodedata
 comment=[unicodedata.normalize('NFKD', i).encode('ascii','ignore') for i in row_list]
 
 # Street Address search in a string of comment
-
 import re
 
 # split every string in cooment into word by word 
@@ -20,9 +19,9 @@ comment_split=[re.split("\s", i) for i in comment]
 
 # parse the comment and select the address
 
-for i in comment:
-  for j in j
-  print (x)
+for i in comment_split[0]:
+    if i.isdigit() and int(re.findall("\d+", i)[0])<10000:
+      print i
 
 
 
