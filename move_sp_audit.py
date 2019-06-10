1326467677d69b5b8eddf178283d243120675bb7
Macchiato0 <50885101+Macchiato0@users.noreply.github.com>
2019-06-10 11:45:34 -0400

    Update move_sp_audit.py

--- move_sp_audit.py
+++ move_sp_audit.py
@@ -1,3 +1,10 @@
 #move the select sp to the audit area
-def move_sp_audit:
+x,y=483320.823,432750.750 
+def move_sp_audit(oid):
+    where="OBJECTID={}".format(oid)
+    cursor=arcpy.da.UpdateCursor(r"Customers & Transformers\Service Point",["SHAPE@XY"],where)
+    for row in cursor:
+        row[0]=func(tlm)
+        cursor.updateRow(row)
+    
     
