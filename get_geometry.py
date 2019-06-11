9e7c52fea1a2d716c6a40aaa19fa001893a65b9c
Macchiato0 <50885101+Macchiato0@users.noreply.github.com>
2019-06-11 12:40:06 -0400

    Update get_geometry.py
    def get_geometry

--- get_geometry.py
+++ get_geometry.py
@@ -1,1 +1,20 @@
+4497 sp without XY processed by DRG
 
+where clause:"DRG is not null and LATITUDE is null and LONGITUDE is null"
+
+#select the sp first  
+def get_geometry():
+    #make sure service points are selected
+    selectedCount = len([int(fid) for fid in arcpy.Describe(r"Customers & Transformers\Service Point").fidset.split(";") if fid != ''])
+    if selectedCount<20000:
+        cursor=arcpy.da.SearchCursor(r"Customers & Transformers\Service Point",["OID@","SHAPE@XY"])
+        A_list=[row for row in cursor]
+        return A_list
+    else:
+        print "select points please"
+      
+      
+    
+    
+  
+  
