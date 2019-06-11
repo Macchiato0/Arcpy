455f8e052f0e8a9c340de60ca1e2d1eda17607d4
Macchiato0 <50885101+Macchiato0@users.noreply.github.com>
2019-06-11 15:09:37 -0400

    Update data_graph.py

--- data_graph.py
+++ data_graph.py
@@ -1,6 +1,24 @@
 DRG create 695785 location on gis
-
+import math
 cursor=arcpy.da.SearchCursor(r"Customers & Transformers\Service Point",["OID@","SHAPE@","LATITUDE","LONGITUDE"]
 
 raw_list=[i for i in cursor]
 
+def get_XY_degree(pt):
+    lat=pt.projectAs(arcpy.SpatialReference(4269)).firstPoint.Y
+    lon=pt.projectAs(arcpy.SpatialReference(4269)).firstPoint.X
+    coor=(lat,lon)
+    return coor
+                             
+(raw_list[0] [2],raw_list[0] [3])
+get_XY_degree(raw_list[0] [1])
+                             
+def distance_degree(pt1,pt2):
+    result=math.sqrt((pt1[0]-pt2[0])**2+(pt1[1]-pt2[1])**2)
+    return result
+                             
+#create 2 list for pt1,pt2
+
+                           
+                             
+                             
