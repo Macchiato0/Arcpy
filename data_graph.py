d158a95cf3421986737135ff9799e95f657ac0bc
Macchiato0 <50885101+Macchiato0@users.noreply.github.com>
2019-06-11 16:33:12 -0400

    Update data_graph.py

--- data_graph.py
+++ data_graph.py
@@ -1,5 +1,9 @@
 DRG create 695785 location on gis
 import math
+import matplotlib
+import numpy as np
+import matplotlib.pyplot as plt
+
 cursor=arcpy.da.SearchCursor(r"Customers & Transformers\Service Point",["OID@","SHAPE@","LATITUDE","LONGITUDE"]
 
 raw_list=[i for i in cursor]
@@ -22,11 +26,27 @@ raw_pt1_geom=[i[1] for i in raw_list]
 pt1_coor=map(get_XY_degree,raw_pt1_geom)
 yy=[[1,2,3],[2,3,4],[7,8,9],[1,3,5]]
 [(i[0],i[1]) for i in yy]
-pt12_coor=[(i[2],i[3]) for i in raw_list]
+pt2_coor=[(i[2],i[3]) for i in raw_list]
                              
-dist=map(distance_degree,pt1_coor,pt12_coor)
+dist=map(distance_degree,pt1_coor,pt2_coor)
+                             
 
+364320 feet a decimal degree
+dist_feet=[364320*i for i in dist]
+oid=[i[0] for i in raw_list]
+tu=lambda a,b : (a,b)                              
+oid_dist_feet=map(tu, oid,dist_feet)
+oid_dist_feet[2]
                              
+#dist over 250 feet
+dist_over_250= [i for i in oid_dist_feet if i[1]>250]
+len(dist_over_250)   40130                         
+dist_over_500= [i for i in oid_dist_feet if i[1]>500]  
+len(dist_over_500)   35858                             
+dist_over_723km= [i for i in oid_dist_feet if i[1]>2.4016e+6]  
+len(dist_over_723km)   30790                          
+                             
+
                            
                              
                              
