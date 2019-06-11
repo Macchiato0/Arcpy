eac7e1e0bed7cb05793a0f113e7b832abd6007b6
Macchiato0 <50885101+Macchiato0@users.noreply.github.com>
2019-06-11 10:08:19 -0400

    Update move_sp_audit.py

--- move_sp_audit.py
+++ move_sp_audit.py
@@ -1,10 +1,26 @@
 #move the select sp to the audit area
-x,y=483320.823,432750.750 
-def move_sp_audit(oid):
-    where="OBJECTID={}".format(oid)
-    cursor=arcpy.da.UpdateCursor(r"Customers & Transformers\Service Point",["SHAPE@XY"],where)
+
+def get_tlm_location(tlm_oid):
+    where="OBJECTID={}".format(tlm_oid)
+    cursor=arcpy.da.UpdateCursor(r"Customers & Transformers\Secondary Transformers",["SHAPE@XY"],where)
     for row in cursor:
-        row[0]=func(tlm)
-        cursor.updateRow(row)
+        result=row[0]
+    return result
+
+def move_sp_audit(oid,tlm_oid):
+    try:
+        coor=get_tlm_location(tlm_oid)
+        print coor
+        where="OBJECTID={}".format(oid)
+        cursor=arcpy.da.UpdateCursor(r"Customers & Transformers\Service Point",["SHAPE@XY"],where)
+        for row in cursor:
+            row[0]=coor
+            cursor.updateRow(row)
+    except:
+        pass
+        
+
+
+ 
     
     
