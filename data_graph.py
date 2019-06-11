c6a027e9c0b2a53cc8738e237388ecb79b5e3fde
Macchiato0 <50885101+Macchiato0@users.noreply.github.com>
2019-06-11 15:25:56 -0400

    Update data_graph.py

--- data_graph.py
+++ data_graph.py
@@ -18,7 +18,15 @@ def distance_degree(pt1,pt2):
     return result
                              
 #create 2 list for pt1,pt2
+raw_pt1_geom=[i[1] for i in raw_list]
+pt1_coor=map(get_XY_degree,raw_pt1_geom)
+yy=[[1,2,3],[2,3,4],[7,8,9],[1,3,5]]
+[(i[0],i[1]) for i in yy]
+pt12_coor=[(i[2],i[3]) for i in raw_list]
+                             
+dist=map(distance_degree,pt1_coor,pt12_coor)
 
+                             
                            
                              
                              
