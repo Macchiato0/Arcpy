b9c007a5a86899bef1d35e213ea497139412f3a4
Macchiato0 <50885101+Macchiato0@users.noreply.github.com>
2019-07-29 13:20:47 -0400

    Update Calculate_XY_functions.py

--- Calculate_XY_functions.py
+++ Calculate_XY_functions.py
@@ -56,8 +56,12 @@ def update_geometry():
         print "select service point first"
         
 
-    
-        
+  #calculate the pt coordinates in meters  
+  def get_XY_meter(pt):
+    lat=pt.projectAs(arcpy.SpatialReference(2008)).firstPoint.Y
+    lon=pt.projectAs(arcpy.SpatialReference(2008)).firstPoint.X
+    coor=(lon,lat)
+    return coor      
       
       
     
