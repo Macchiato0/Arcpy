ff05a48e94e6c14d92f370bfba9b50e2d9d1819a
Macchiato0 <50885101+Macchiato0@users.noreply.github.com>
2019-07-31 11:55:16 -0400

    Update Script_2_check_null_address.py

--- Script_2_check_null_address.py
+++ Script_2_check_null_address.py
@@ -5,3 +5,17 @@ for row in cursor:
     cur=arcpy.da.SearchCursor("ELECDIST.ServiceAddress",["STREET"],where)
     st=[r[0] for r in cur]
     if len(st)<1: null_address.append(row[0])
+'''
+print the address of sp
+'''
+cursor=arcpy.da.SearchCursor("Service Point selection",["OID@"])
+oid_list=[]
+for row in cursor:
+    oid_list.append(int(row[0]))
+    
+    
+for i in oid_list:
+    where="SERVICEPOINTOBJECTID={}".format(i)
+    cursor=arcpy.da.SearchCursor("ELECDIST.ServiceAddress",["SERVICEPOINTOBJECTID","STREET", "CITY", "POSTALCODE"], where)
+    for row in cursor:
+        print str(row[0])+","+str(row[1])+","+str(row[2])+","+str(row[3])[0:5]
