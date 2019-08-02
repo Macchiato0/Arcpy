71103424a0c2e821e237be006a1f4b8b2d105a5e
Macchiato0 <50885101+Macchiato0@users.noreply.github.com>
2019-08-02 12:41:23 -0400

    Update Has_street_function.py

--- Has_street_function.py
+++ Has_street_function.py
@@ -1,10 +1,18 @@
 #function to test if the address in address table and extract the matched SP OID
 def is_intable(x): 
-    cursor=arcpy.da.SearchCursor("ELECDIST.ServiceAddress",["SERVICEPOINTOBJECTID"],x)
+    where="SERVICEPOINTOBJECTID={}".format(x)
+    cursor=arcpy.da.SearchCursor("ELECDIST.ServiceAddress",["STREET"],where)
     id=[row[0] for row in cursor]
-    if len(id)>1:
-        print 'Service has address.'
+    if len(id)>0:
+       return 1
     else:
-        print 'Service has no address'
+        return 0
+        
+        
+cursor=arcpy.da.SearchCursor("Service Point selection",["OID@"])
 
-
+oid=[i[0] for i in cursor]
+oid_no_street=[]
+for i in oid:
+    if is_intable(i)==0:
+        oid_no_street.append(i)
