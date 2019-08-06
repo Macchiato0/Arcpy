77ec43972104be78964889ed4ec4b4f03e1d3bb0
Macchiato0 <50885101+Macchiato0@users.noreply.github.com>
2019-08-06 16:25:48 -0400

    Update merge_polygon.py

--- merge_polygon.py
+++ merge_polygon.py
@@ -18,8 +18,25 @@ subid=[str(i[0]) for i in cursor]
 
 #extract polygons belongs to every substations
 #["SUBSTATION","HDQ","SHAPE@"]
+
 for i in subid:
     where="SUBSTATIONID={}".format(i)
     cursor=arcpy.da.SearchCursor(r'E:\Data\yfan\Connection to dgsep011.sde\ELECDIST.ElectricDist_GO\ELECDIST.Boundary_Feeder_GO',["SHAPE@"])
     plgongs=[i[0] for i in cursor]
-    
+    sub_poly=reduce(lambda a,b : a.union(b),plgongs)
+'''    
+cursor = arcpy.da.InsertCursor(r'E:\Data\yfan\sand_box.gdb\ELECDIST_Dissolve',["SHAPE@"]) 
+cursor.insertRow([pg1])
+
+
+
+
+
+#test sec_oh
+
+
+
+cursor=arcpy.da.SearchCursor(r'E:\Data\yfan\sand_box.gdb\ELECDIST_Dissolve',["*"])
+for i in cursor:
+    print i
+'''
