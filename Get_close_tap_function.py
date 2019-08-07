3fccf0307c23cfd9ff9c14628abccdb96f0162c6
Macchiato0 <50885101+Macchiato0@users.noreply.github.com>
2019-08-07 16:36:26 -0400

    Update Get_close_tap_function.py

--- Get_close_tap_function.py
+++ Get_close_tap_function.py
@@ -5,21 +5,23 @@ feederid in ('129802','051101','051102','060203','060204','140701','140702','026
 
 import itertools
 
-feederid=['129802','051101','051102','060203','060204','140701','140702','026501','026502','077201','077202']
+
 
 #delete the lable of tap points to close to each other
 def get_close_tap(feederid): 
-    where="feederid='{}'".format(i)
+    where="feederid='{}'".format(feederid)
     cursor=arcpy.da.SearchCursor("Tap Dots, T-points, & Wire Changes selection",["OID@","SHAPE@","LCP"],where)
     rows=[q for q in cursor]
     rows2=[]
     #find the taps with a label
-    tap=[]
+    tap1=[]
+    tap2=[]
+    tap3=[]
     for j in rows:     
-        where="FEATUREID={}".format(j[0])
-        cursor1=arcpy.da.SearchCursor(r'Misc Network Features\Feature Linked Anno\MiscNetworkFeature_Label_300',["SHAPE@"],where)
-        cursor2=arcpy.da.SearchCursor(r'Misc Network Features\Feature Linked Anno\MiscNetworkFeature_Label_3200',["SHAPE@"],where)
-        cursor3=arcpy.da.SearchCursor(r'Misc Network Features\Feature Linked Anno\MiscNetworkFeature_Label_600',["SHAPE@"],where)
+        where2="FEATUREID={}".format(j[0])
+        cursor1=arcpy.da.SearchCursor(r'Misc Network Features\Feature Linked Anno\MiscNetworkFeature_Label_300',["SHAPE@"],where2)
+        cursor2=arcpy.da.SearchCursor(r'Misc Network Features\Feature Linked Anno\MiscNetworkFeature_Label_3200',["SHAPE@"],where2)
+        cursor3=arcpy.da.SearchCursor(r'Misc Network Features\Feature Linked Anno\MiscNetworkFeature_Label_600',["SHAPE@"],where2)
         #apply try keywords to escape empty cursor error
         try:
             lable_shp300=[r1[0] for r1 in cursor1]
@@ -37,10 +39,23 @@ def get_close_tap(feederid):
         if len(lable)>0:
             rows2.append(j)        
     for x,y in itertools.combinations(rows2, 2):
-        if x[1].distanceTo(y[1])<400:
-            tap.append(x[0])
-            tap.append(y[0])
-    tapr=list(set(tap))
-    return tapr    
+        meters=x[1].distanceTo(y[1])
+        if 300<=meters<=400:
+            tap3.append(x[0])
+            tap3.append(y[0])
+        elif 200<=meters<300:
+            tap2.append(x[0])
+            tap2.append(y[0])
+        else:
+            tap1.append(x[0])
+            tap1.append(y[0])
+    tapr1=list(set(tap1))
+    tapr2=list(set(tap2))
+    tapr3=list(set(tap3))
+    tapr=[tapr1,tapr2,tapr3]
+    return tapr 
    
 '''
+test taps
+cursor=arcpy.da.SearchCursor("Tap Dots, T-points, & Wire Changes selection",["OID@"])
+l1=[i[0] for i in cursor]
