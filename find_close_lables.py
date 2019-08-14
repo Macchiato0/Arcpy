271bd2ebbceecc94950172e1e1cf651f8b3e260b
Macchiato0 <50885101+Macchiato0@users.noreply.github.com>
2019-08-14 12:13:48 -0400

    Update find_close_lables.py

--- find_close_lables.py
+++ find_close_lables.py
@@ -3,43 +3,51 @@ select lable cluster
 
 '''
 
-# import itertools
+import itertools
 
 
 
 #delete the lable of tap points to close to each other
 
 def get_close_lable(feederid): 
-    layers=[r'Misc Network Features\Feature Linked Anno\MiscNetworkFeature_Label_300',
-    r'Misc Network Features\Feature Linked Anno\MiscNetworkFeature_Label_3200',
-    r'Misc Network Features\Feature Linked Anno\MiscNetworkFeature_Label_600',
-    r'Customers & Transformers\Feature Linked Anno\PrimaryMeter_Label_300',
-    r'Customers & Transformers\Feature Linked Anno\PrimaryMeter_Label_600',
-    r'Customers & Transformers\Feature Linked Anno\PrimaryMeter_Label_3200',
-    r'Devices\Protective Devices & Switches\Feature Linked Anno\DynamicProtectiveDevice_Label_300',
-    r'Devices\Protective Devices & Switches\Feature Linked Anno\DynamicProtectiveDevice_Label_600',
-    r'Devices\Protective Devices & Switches\Feature Linked Anno\DynamicProtectiveDevice_Label_3200',
-    r'Devices\Protective Devices & Switches\Feature Linked Anno\Fuse_Label_300',
-    r'Devices\Protective Devices & Switches\Feature Linked Anno\Fuse_Label_600',
-    r'Devices\Protective Devices & Switches\Feature Linked Anno\Fuse_Label_3200',
-    r'Devices\Protective Devices & Switches\Feature Linked Anno\Switch_Label_300',
-    r'Devices\Protective Devices & Switches\Feature Linked Anno\Switch_Label_600',
-    r'Devices\Protective Devices & Switches\Feature Linked Anno\Switch_Label_3200',
-    r'Devices\Primary Devices\Feature Linked Anno\PFCorrectingEquipment_Label_300',
-    r'Devices\Primary Devices\Feature Linked Anno\PFCorrectingEquipment_Label_600',
-    r'Devices\Primary Devices\Feature Linked Anno\PFCorrectingEquipment_Label_3200',
-    r'Devices\Primary Devices\Feature Linked Anno\Transformer_Label_300',
-    r'Devices\Primary Devices\Feature Linked Anno\Transformer_Label_600',
-    r'Devices\Primary Devices\Feature Linked Anno\Transformer_Label_3200',
-    r'Devices\Primary Devices\Feature Linked Anno\VoltageRegulator_Label_300',
-    r'Devices\Primary Devices\Feature Linked Anno\VoltageRegulator_Label_600',
-    r'Devices\Primary Devices\Feature Linked Anno\VoltageRegulator_Label_3200']    
+    layers=[
+    r'Devices\Protective Devices & Switches\Fuse',
+    r'Devices\Protective Devices & Switches\Switch',
+    r'Devices\Protective Devices & Switches\Dynamic Protective Device',
+    r'Devices\Primary Devices\Regulators & Boosters',
+    r'Devices\Primary Devices\Isolator',
+    r'Devices\Primary Devices\Capacitors'
+    ]    
     where="feederid='{}'".format(feederid)
-    oid,shp=[],[]
+    oid,shp,lay=[],[],[]
     for layer in layers:
         cursor=arcpy.da.SearchCursor(layer,["OID@","SHAPE@"],where)
         for i in cursor:
             oid.append(i[0])
-            shp.append(i[1])     
+            shp.append(i[1]) 
+            lay.append(layer) 
+    cursor=arcpy.da.SearchCursor(r'Misc Network Features\Tap Dots, T-points, & Wire Changes',["OID@","SHAPE@"],where)
+    for row in cursor:
+        where2="FEATUREID={}".format(row[0])
+        cursor1=arcpy.da.SearchCursor(r'Misc Network Features\Feature Linked Anno\MiscNetworkFeature_Label_300',["FEATUREID"],where2)
+        for r1 in cursor1:
+            if r1:
+                oid.append(row[0])
+                shp.append(row[1])
+                lay.append(layer)            
+        cursor2=arcpy.da.SearchCursor(r'Misc Network Features\Feature Linked Anno\MiscNetworkFeature_Label_3200',["FEATUREID"],where2)
+        for r2 in cursor2:
+            if r2:
+                oid.append(row[0])
+                shp.append(row[1])
+                lay.append(layer)            
+        cursor3=arcpy.da.SearchCursor(r'Misc Network Features\Feature Linked Anno\MiscNetworkFeature_Label_600',["FEATUREID"],where2)
+        for r3 in cursor1:
+            if r3:
+                oid.append(row[0])
+                shp.append(row[1])
+                lay.append(layer)        
+    #FIND THE CLUSTER
+    Fuse_layer=           
     return oid 
 '''
