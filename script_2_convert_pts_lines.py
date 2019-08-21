9024aa84e5d872d0e0fb1ec6101d0268b182a3ac
Macchiato0 <50885101+Macchiato0@users.noreply.github.com>
2019-08-21 14:58:41 -0400

    Update script_2_convert_pts_lines.py

--- script_2_convert_pts_lines.py
+++ script_2_convert_pts_lines.py
@@ -5,7 +5,7 @@ t_pt1=arcpy.Point(594307.222,543774.535)
 t_pt2=arcpy.Point(594317.222,543784.535)
 t_pl=arcpy.Polyline(arcpy.Array([t_pt1,t_pt2]))
 """
-
+# convert pt and transformer pts to lines
 # first point
 # make a list of last point for lines
 # service points
@@ -65,4 +65,51 @@ for i in cursor:
 
 # arcpy.DeleteRows_management('Sec_OH')
 
+########################################################################################################################################
+########################################################################################################################################
 
+
+# convert pt and tap pts to lines
+
+cursor=arcpy.da.SearchCursor("Service Point selection",["OID@","SHAPE@","FEEDERID"])
+pt_list=[]
+oid_list=[]
+tap_list=[]
+
+for row in cursor:
+    pt_list.append(row[1])
+    oid_list.append(row[0])
+    tap_list.append(int(row[2]))
+
+    
+# collect tap points as the first point of a line
+pt_0=[]
+pt0_phase=[]
+for i in tap_list:
+    where="OBJECTID={}".format(i)
+    cursor=arcpy.da.SearchCursor('Misc Network Features\\Tap Dots, T-points, & Wire Changes',["SHAPE@","PHASEDESIGNATION"],where)
+    for row in cursor:
+        pt_0.append(row[0])
+        pt0_phase.append(row[1])
+        
+        
+# zip point 0, point1, and the phase
+pts_phase=zip(pt_0,pt_list,pt0_phase)
+
+for i in pts_phase:
+    t_pt1,t_pt2=i[0].firstPoint,i[1].firstPoint
+    xyz=i[2]
+    t_pl=arcpy.Polyline(arcpy.Array([t_pt1,t_pt2]))     
+    cursor = arcpy.da.InsertCursor(r'E:\Data\yfan\sand_box.gdb\Sec_OH',["SHAPE@","PHASEDESIGNATION"]) 
+    cursor.insertRow([t_pl,xyz])
+    
+# test sec_oh
+
+cursor=arcpy.da.SearchCursor(r'E:\Data\yfan\sand_box.gdb\Sec_OH',["*"])
+n=0
+for i in cursor:
+    n=n+1
+    print i
+    
+    
+    
