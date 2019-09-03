c16b83ba3d50ce4163b3067c325887386b57b0f4
Macchiato0 <50885101+Macchiato0@users.noreply.github.com>
2019-09-03 09:57:43 -0400

    Update script_2_match_zipcode.py

--- script_2_match_zipcode.py
+++ script_2_match_zipcode.py
@@ -17,6 +17,7 @@ groups_zipcode=[i for i in list(set(zip_list)) if i[-4] not '0000']
 #remove the zipcode+4 with 0000 as the block code
 #0000 is the address not verified by usps service
 #last 4 digit identifies a block, a segment of street, or a certain area for delivering purpose
+import itertools
 
 def get_pt(x): #x is oid of sp
     where='Objectid={}'.format(x)
@@ -26,13 +27,16 @@ def get_pt(x): #x is oid of sp
     return p
 
 
-def cal_meter(l): # l is a list of points features
-    delta_meter=[
-
-
+incosistant_pt={}
 for i in groups_zipcode:
     pts_oid=[j[0] for j in list_sp if j[1]==i]
     pts_shp=[get_pt(p) for p in pts_oid]
-    
-    
+    d_pt_oid = dict(zip(pts_oid, pts_shp))                       
+    pair_pt=list(itertools.combinations(pts_oid,2))                       
+    for z in pair_pt:
+        m=d_pt_oid(z[0]).distanceTo(d_pt_oid(z[1]))
+        if m>= 1000:     #distance between 2 sp with same zip+4 is farer than 1000 meter               
+            incosistant_pt(z)=m
+                           
+                           
 
