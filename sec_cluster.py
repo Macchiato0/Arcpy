ffd45083791bf5413850f956e99ea640a80f59d3
Macchiato0 <50885101+Macchiato0@users.noreply.github.com>
2019-09-20 10:23:29 -0400

    Update sec_cluster.py

--- sec_cluster.py
+++ sec_cluster.py
@@ -1,36 +1,37 @@
-cursor=arcpy.da.SearchCursor('E:\\Data\\yfan\\Connection to dgsep011.sde\\ELECDIST.ElectricDist\\ELECDIST.SecOHElectricLineSegment',["OID@","SHAPE@"],"feederid='{}'".format(self.fid))
+fid='011003'
+cursor=arcpy.da.SearchCursor('E:\\Data\\yfan\\Connection to dgsep011.sde\\ELECDIST.ElectricDist\\ELECDIST.SecOHElectricLineSegment',["OID@","SHAPE@"],"feederid='{}'".format(fid))
 line_shp={k:v for (k,v) in cursor}
-cursor=arcpy.da.SearchCursor('E:\\Data\\yfan\\Connection to dgsep011.sde\\ELECDIST.ElectricDist\\ELECDIST.Transformer',["OID@","SHAPE@","TLM"],"feederid='{}'".format(self.fid))
+cursor=arcpy.da.SearchCursor('E:\\Data\\yfan\\Connection to dgsep011.sde\\ELECDIST.ElectricDist\\ELECDIST.Transformer',["OID@","SHAPE@","TLM"],"feederid='{}'".format(fid))
 tlm_shp={k:(v1,v2) for (k,v1,v2) in cursor}
-cursor=arcpy.da.SearchCursor('E:\\Data\\yfan\\Connection to dgsep011.sde\\ELECDIST.ElectricDist\\ELECDIST.ServicePoint',["OID@","SHAPE@","DEVICELOCATION"],"feederid='{}'".format(self.fid))
+cursor=arcpy.da.SearchCursor('E:\\Data\\yfan\\Connection to dgsep011.sde\\ELECDIST.ElectricDist\\ELECDIST.ServicePoint',["OID@","SHAPE@","DEVICELOCATION"],"feederid='{}'".format(fid))
 sp_shp={k:(v1,v2) for (k,v1,v2) in cursor}
 cluster=[]        
-cluster1=dict([self.line_shp.popitem()])
-cluster2=self.cluster1 #temp store of clustered lines    
-line_shp_temp=copy.deepcopy(self.line_shp)
+cluster1=dict([line_shp.popitem()])
+cluster2=cluster1 #temp store of clustered lines    
+line_shp_temp=copy.deepcopy(line_shp)
         
 # recursive method:
 # search lines linked to [line1] 
 
-def find_line(self): #find the linked lines, lines=a.sub_cluster1
+def find_line(): #find the linked lines, lines=a.sub_cluster1
     line_cont={}
-    for k1 in self.line_shp_temp:
-        for k2 in self.cluster1:
-            if self.cluster1[k2].distanceTo(self.line_shp[k1])==0:
-                line_cont[k1]=self.line_shp.pop(k1)
-                self.line_shp_temp=copy.deepcopy(self.line_shp)
+    for k1 in line_shp_temp:
+        for k2 in cluster1:
+            if cluster1[k2].distanceTo(line_shp[k1])==0:
+                line_cont[k1]=line_shp.pop(k1)
+                line_shp_temp=copy.deepcopy(line_shp)
     if len(line_cont)>0:
-        self.cluster2.update(line_cont)
-        self.cluster1={}
-        self.cluster1.update(line_cont)
-        self.find_line(self)
-    elif len(line_cont)==0 and len(self.line_shp)>0:
-        self.cluster.append(self.cluster2)
-        self.cluster1=dict([self.line_shp.popitem()])
-        self.line_shp_temp=copy.deepcopy(self.line_shp)
-        self.cluster2={}
-        self.cluster2.update(self.cluster1)
-        self.find_line(self)
+        cluster2.update(line_cont)
+        cluster1={}
+        cluster1.update(line_cont)
+        find_line()
+    elif len(line_cont)==0 and len(line_shp)>0:
+        cluster.append(cluster2)
+        cluster1=dict([line_shp.popitem()])
+        line_shp_temp=copy.deepcopy(line_shp)
+        cluster2={}
+        cluster2.update(cluster1)
+        find_line()
     else:
-        return self.cluster
+        return cluster
             
