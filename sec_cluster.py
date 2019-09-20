7077a6d902cd7c64a9e07d34e8c584ebea45a1dc
Macchiato0 <50885101+Macchiato0@users.noreply.github.com>
2019-09-20 14:59:43 -0400

    Update sec_cluster.py

--- sec_cluster.py
+++ sec_cluster.py
@@ -45,17 +45,50 @@ def find_line(lists):#lists=cluster1
         cluster.append(cluster1)
         return len(cluster)
 
-
+find_line(cluster1)
 
 #add sec underground to sec overhead network clusters 
 fid='011003'
-cursor=arcpy.da.SearchCursor('E:\\Data\\yfan\\Connection to dgsep011.sde\\ELECDIST.ElectricDist\\ELECDIST.ELECDIST.SecUGElectricLineSegment',["OID@","SHAPE@"],"feederid='{}'".format(fid))
+cursor=arcpy.da.SearchCursor('E:\\Data\\yfan\\Connection to dgsep011.sde\\ELECDIST.ElectricDist\\ELECDIST.SecUGElectricLineSegment',["OID@","SHAPE@"],"feederid='{}'".format(fid))
 
 line_shp=[[i[0],[(int(i[1].firstPoint.X),int(i[1].firstPoint.Y)),(int(i[1].lastPoint.X),int(i[1].lastPoint.Y))]] for i in cursor]
 
+un_cluster=[]        
+un_cluster1=[line_shp[0]]
+un_cluster2=[]
+un_cluster3=[]
     
-
-
+def find_line(lists):#lists=un_cluster1
+    global un_cluster
+    global un_cluster2
+    global un_cluster1
+    global un_cluster3
+    global line_shp
+    for l in lists:
+        if l in line_shp:
+            line_shp.remove(l)
+    for i in line_shp:
+        for j in lists:
+            if [a for a in i[1] if a in j[1]]:  
+                if i not in lists:
+                    un_cluster2.append(i)
+    if len(un_cluster2)>0:
+        un_cluster3=[]
+        for h in un_cluster2:
+            un_cluster1.append(h)
+            un_cluster3.append(h)
+        un_cluster2=[]
+        #print cluster1
+        find_line(un_cluster3)
+    elif len(line_shp)>0:
+        un_cluster.append(un_cluster1)
+        un_cluster3=[]
+        un_cluster1=[line_shp[0]]
+        find_line(un_cluster1)
+    else:
+        un_cluster.append(un_cluster1)
+        return len(un_cluster)
+find_line(un_cluster1)
         
         
         
