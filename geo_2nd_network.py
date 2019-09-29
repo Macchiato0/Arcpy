ba344819b8415995bfd95448dc150d6c9ba14494
Macchiato0 <50885101+Macchiato0@users.noreply.github.com>
2019-09-29 19:08:48 -0400

    Update and rename geo_set_network.py to geo_2nd_network.py

--- geo_2nd_network.py
+++ geo_2nd_network.py
@@ -11,7 +11,14 @@ line_shp=[[i[0],[(int(i[1].firstPoint.X),int(i[1].firstPoint.Y)),(int(i[1].lastP
 #line_shp[0]
 #[11341189, [(559725, 230913), (559714, 230922)], u'Aluminum', u'4TX']
 #i[1] [(559725, 230913), (559714, 230922)]
-
+#create points based based on line_shp,oid is unique
+pt_shp=[]
+n=0
+for i in line_shp:
+  for j in i[1]:
+    n+=1
+    row=[n,j]
+    pt_shp.append(row)
 #swap coordinate tuple (x,y) with point id in pt_shp   
 n=0
 for i in line_shp:
@@ -21,16 +28,7 @@ for i in line_shp:
         
 #pt_shp[0]
 #[1, (559725, 230913)]
-
-#create points based based on line_shp,oid is unique
-pt_shp=[]
-n=0
-for i in line_shp:
-  for j in i[1]:
-    n+=1
-    row=[n,j]
-    pt_shp.append(row)
-
+#all points data are in the form [id,(x,y)]
 #create list of service point
 cursor=arcpy.da.SearchCursor('E:\\Data\\yfan\\Connection to dgsep011.sde\\ELECDIST.ElectricDist\\ELECDIST.ServicePoint',["SHAPE@","DEVICELOCATION"],"feederid='{}'".format(fid))
 sp_shp=[[i[1],(int(i[0].firstPoint.X),int(i[0].firstPoint.Y))] for i in cursor]
@@ -39,7 +37,13 @@ sp_shp=[[i[1],(int(i[0].firstPoint.X),int(i[0].firstPoint.Y))] for i in cursor]
 cursor=arcpy.da.SearchCursor('E:\\Data\\yfan\\Connection to dgsep011.sde\\ELECDIST.ElectricDist\\ELECDIST.Transformer',["SHAPE@","TLM"],"feederid='{}'".format(fid))
 tlm_shp=[[str(i[1]),(int(i[0].firstPoint.X),int(i[0].firstPoint.Y))] for i in cursor]
 
-#create a Quicksort function to sort 3 points list based on their X value (longitude)
+#sort pt,sp, and tlm @ Py_sort_search_data_structure/quick_sort_point.py 
+
+#search and match pt with sp and tlm @ Py_sort_search_data_structure/binary_search_point.py
+
+
+
+
 
 
 
