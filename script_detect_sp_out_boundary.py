798bb59c96f89194f79230a8e061f72b117ed853
Macchiato0 <50885101+Macchiato0@users.noreply.github.com>
2019-10-10 20:30:40 -0400

    Update script_detect_sp_out_boundary.py

--- script_detect_sp_out_boundary.py
+++ script_detect_sp_out_boundary.py
@@ -1,78 +1,17 @@
 #ctrl+c to stop calculation
 import csv
-'''
-file_name4='E:\\Data\\yfan\\PyModules\\sp_adrs_list.csv'
-with open(file_name4, 'wb') as csvfile:
-  filewriter = csv.writer(csvfile,delimiter=',',quotechar='"',quoting=csv.QUOTE_MINIMAL)
-  header=["OID","FEEDERID","SHAPE","OID","STEET","CITY","WHQ"]
-  filewriter.writerow(header)
-  cursor=arcpy.da.SearchCursor(r'E:\Data\yfan\Connection to dgsep011.sde\ELECDIST.ElectricDist\ELECDIST.ServicePoint',["OID@","FeederID","SHAPE"],"ObjectID<50000")
-  for i in cursor:
-    oid=i[0]
-    fid=i[1]
-    shp=i[2]
-    sp_id=i[0]
-    where="SERVICEPOINTOBJECTID={}".format(oid)
-    cur=arcpy.da.SearchCursor(r'E:\Data\yfan\Connection to dgsep011.sde\ELECDIST.ServiceAddress',["STREET","CITY","WORKHEADQUARTERS"],where)
-    for r in cur:
-      st=r[0]
-      ct=r[1]
-      wq=r[2]
-      filewriter.writerow([oid,fid,shp,sp_id,st,ct,wq]) 
-'''     
-     
-    
-    
+
 # create a dictionary of work HQ polygon object and their id
 cursor=arcpy.da.SearchCursor(r'E:\Apps\Application Launch\Electric\CVMWNT0146_GISLand.sde\GISLand.DBO.Land\GISLand.DBO.ElectricDistributionWHQ'
 ,["BOUNDARYNAMECD","SHAPE@"])
 wkhd_plyg=dict ([(i[0],i[1]) for i in cursor])
 wkhd_plyg
-'''
-file_name1='E:\\Data\\yfan\\PyModules\\sp_list.csv'
-with open(file_name1, 'wb') as csvfile:
-  filewriter = csv.writer(csvfile,delimiter=',',quotechar='"',quoting=csv.QUOTE_MINIMAL)
-  header=["OBID","ServiceID","FEEDERID","SHAPE"]
-  filewriter.writerow(header)
-  cursor=arcpy.da.SearchCursor(r'E:\Data\yfan\Connection to dgsep011.sde\ELECDIST.ElectricDist\ELECDIST.ServicePoint',["OID@","FeederID","SHAPE"])
-  for i in cursor:
-    oid=i[0]
-    spid=i[0]
-    fid=i[1]
-    shp=i[2]
-    filewriter.writerow([oid,spid,fid,shp]) 
-    cursor=arcpy.da.SearchCursor("SP",['SP.ServiceID', 'SP.FEEDERID', 'SP.SHAPE','SP_adrs.SERVICEPOINTOBJECTID', 'SP_adrs.STREET', 'SP_adrs.CITY', 'SP_adrs.WORKHEADQUARTERS'])
-    
-'''
     
 cursor=arcpy.da.SearchCursor(r'E:\Data\yfan\Connection to dgsep011.sde\ELECDIST.ElectricDist\ELECDIST.ServicePoint',["OID@","FeederID","SHAPE@"])    
 sp={}
 for i in cursor:
   sp[i[0]]=[i[1],i[2]]
     
-
-    
-    
-    
-    
-    
-    
-'''  
-file_name2='E:\\Data\\yfan\\PyModules\\sp_address.csv'
-with open(file_name2, 'wb') as csvfile:
-  filewriter = csv.writer(csvfile,delimiter=',',quotechar='"',quoting=csv.QUOTE_MINIMAL)
-  header=["SP","SERVICEPOINTOBJECTID","STREET","CITY","WORKHEADQUARTERS"]
-  filewriter.writerow(header)
-  cursor=arcpy.da.SearchCursor(r'E:\Data\yfan\Connection to dgsep011.sde\ELECDIST.ServiceAddress',["SERVICEPOINTOBJECTID","STREET","CITY","WORKHEADQUARTERS"])
-  for i in cursor:
-    obid=i[0]
-    sp_oid=i[0]
-    st=i[1]
-    ct=i[2]
-    wh=i[3]
-    filewriter.writerow([obid,sp_oid,st,ct,wh])       
-'''
-
 cursor=arcpy.da.SearchCursor(r'E:\Data\yfan\Connection to dgsep011.sde\ELECDIST.ServiceAddress',["SERVICEPOINTOBJECTID","STREET","CITY","WORKHEADQUARTERS"],"SERVICEPOINTOBJECTID is not null")    
 sp_adrs={}
 for r in cursor:
@@ -97,19 +36,42 @@ print len(sp)
 print sp[1],sp[20],sp[202][1]
 
 
+
+
+out_bound=[]
 for k in sp:
-  for k1 in wkhd_plyg:
-    if wkhd_plyg[k1].contains(sp[k][1]):
-      sp[k].append(k1)
-
+  try:
+    whq=sp[k][4]
+    if not wkhd_plyg[whq].contains(sp[k][1]):
+      out_bound.append(k)
+  except:
+    pass
       
-      
-print sp[1],sp[20],sp[202]
+print len(out_bound)   
+out_bound[10000]
 
+real_sp_wh={}
+for i in out_bound:
+  for k in wkhd_plyg:
+    if wkhd_plyg[k].contains(sp[i][1]):
+      real_sp_wh[i]=k
+    
+
+print real_sp_wh[1324595]
+
+
+for k in sp:
+  sp[k].remove(sp[k][1])
+   
+    
+    
 import json
 
 with open('E:\\Data\\yfan\\PyModules\\sp_hq.json', 'w') as fp:
   json.dump(sp, fp)
+  
+with open('E:\\Data\\yfan\\PyModules\\sp_real_hq.json', 'w') as fp:
+  json.dump(real_sp_wh, fp)
   
 with open('E:\\Data\\yfan\\PyModules\\sp_hq.json') as f:
   data = json.load(f)
