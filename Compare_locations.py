0fc080d89eb13a92c9efdf394457a5ca26c796de
Macchiato0 <50885101+Macchiato0@users.noreply.github.com>
2019-11-25 16:38:38 -0500

    Update Compare_locations.py

--- Compare_locations.py
+++ Compare_locations.py
@@ -1,19 +1,42 @@
 #this script will calculate the distance between sp on CE ELlectric with sp on Outage_Map
 
-#join severice point with service address table on objectid, join servie point with customer location on account number
+#join severice point with service address table on objectid, join servie point with customer location on account number (only keep matched)
 
-
-acc_l,geo_l=[],[]
-cursor1=arcpy.da.SearchCursor("CUSTOMER_LOCATION",["ACCOUNT","SHAPE@"])
+acc_geo_0={}
+cursor1=arcpy.da.SearchCursor('CUSTOMER_LOCATION',["ACCOUNT","SHAPE@"])
+n=0
 for row in cursor1:
-  acc_l.append(row[0])
-  geo_l.append(row[1])
+  acc_geo_0[row[0]]=row[1]
+  n=n+1
+  if n%38000==0:
+    print n
+    
+    
+print len(acc_geo_0)    
 
-for i in range(len(acc_l)):
-  acc=acc_l[i]
-  geo=geo_l[i]
-  where="ELECDIST.ServiceAddress.ACCOUNTNUMBER='{}'".format(acc)
-  cursor=arcpy.da.SearchCursor(r"Customers & Transformers\Service Point",["SHAPE@"])
-  for p in cursor:
-    meter=p[0].distanceTo(geo)
-  print meter  
+
+
+
+acc_geo_2={}
+cursor2=arcpy.da.SearchCursor(r"Customers & Transformers\Service Point",["ELECDIST.ServiceAddress.ACCOUNTNUMBER","SHAPE@"])
+n=0
+for row in cursor2:
+  acc_geo_2[row[0]]=row[1]
+  n=n+1
+  if n%38000==0:
+    print n
+
+print len(acc_geo_2)     
+  
+
+acc_dist={}  
+for k in acc_geo_0:
+  try:
+    dist=acc_geo_0[k].distanceTo(acc_geo_2[k])
+    acc_dist[k]=dist
+  except:
+    pass
+  
+print len(acc_dist)   
+
+
