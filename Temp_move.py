298124bc3fd8600bc6bfc28898f6de0ac92b750a
Macchiato0 <50885101+Macchiato0@users.noreply.github.com>
2020-01-14 10:26:01 -0500

    Update Temp_move.py

--- Temp_move.py
+++ Temp_move.py
@@ -1,28 +1,20 @@
-def move_sp(a):
+def temp_move(a):
     workspace = r'E:\Data\yfan\Connection to dgsep011.sde'
     edit = arcpy.da.Editor(workspace)
     edit.startEditing(False, True)
     edit.startOperation()
     
-    where="OBJECTID={}".format(a)
-    cursor=arcpy.da.SearchCursor(r'E:\Data\yfan\Connection to dgsep011.sde\ELECDIST.ElectricDist\ELECDIST.ServicePoint',["TLM"],where)
+    where="DEVICELOCATION={}".format(a)
+    cursor=arcpy.da.SearchCursor(r'E:\Data\yfan\sand_box.gdb\sp107502',["SHAPE@"],where)
     for row in cursor:
-        tlm=str(row[0])
+        sp_107502=row[0]        
         
-    where="TLM='{}'".format(tlm)
-    cursor=arcpy.da.SearchCursor(r'E:\Data\yfan\Connection to dgsep011.sde\ELECDIST.ElectricDist\ELECDIST.Transformer',["SHAPE@"],where)
-    for row in cursor:
-        pt=row[0]
-        
-    where="OBJECTID={}".format(a)
+    where="DEVICELOCATION={}".format(a)
     cursor=arcpy.da.UpdateCursor(r'E:\Data\yfan\Connection to dgsep011.sde\ELECDIST.ElectricDist\ELECDIST.ServicePoint',["SHAPE@"],where)
     for row in cursor:
-        row[0]=pt
+        row[0]=sp_107502
         cursor.updateRow(row)
         
     edit.stopOperation()
 
     
-cursor=arcpy.da.SearchCursor("Service Point selection",["OID@"])
-for i in cursor:
-    move_sp(i[0])
