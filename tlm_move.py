56754aa0dd662056a7f979152476ed279a804bb3
Macchiato0 <50885101+Macchiato0@users.noreply.github.com>
2020-01-14 10:25:46 -0500

    Update tlm_move.py

--- tlm_move.py
+++ tlm_move.py
@@ -1,18 +1,18 @@
-def temp_move(a):
+def tlm_move(a):
     workspace = r'E:\Data\yfan\Connection to dgsep011.sde'
     edit = arcpy.da.Editor(workspace)
     edit.startEditing(False, True)
     edit.startOperation()
     
-    where="DEVICELOCATION={}".format(a)
-    cursor=arcpy.da.SearchCursor(r'E:\Data\yfan\sand_box.gdb\sp107502',["SHAPE@"],where)
+    where="TLM='{}'".format(a)
+    cursor=arcpy.da.SearchCursor(r'E:\Data\yfan\sand_box.gdb\tlm107502',["SHAPE@"],where)
     for row in cursor:
-        sp_107502=row[0]        
+        tlm_107502=row[0]        
         
-    where="DEVICELOCATION={}".format(a)
-    cursor=arcpy.da.UpdateCursor(r'E:\Data\yfan\Connection to dgsep011.sde\ELECDIST.ElectricDist\ELECDIST.ServicePoint',["SHAPE@"],where)
+    where="TLM={}".format(a)
+    cursor=arcpy.da.UpdateCursor(r'E:\Data\yfan\Connection to dgsep011.sde\ELECDIST.ElectricDist\ELECDIST.Transformer',["SHAPE@"],where)
     for row in cursor:
-        row[0]=sp_107502
+        row[0]=tlm_107502
         cursor.updateRow(row)
         
     edit.stopOperation()
