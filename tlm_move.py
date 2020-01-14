e7ba3c7444e690caf12ae17b8822319a14e6e79f
Macchiato0 <50885101+Macchiato0@users.noreply.github.com>
2020-01-14 10:26:52 -0500

    Update tlm_move.py

--- tlm_move.py
+++ tlm_move.py
@@ -9,7 +9,7 @@ def tlm_move(a):
     for row in cursor:
         tlm_107502=row[0]        
         
-    where="TLM={}".format(a)
+    where="TLM='{}'".format(a)
     cursor=arcpy.da.UpdateCursor(r'E:\Data\yfan\Connection to dgsep011.sde\ELECDIST.ElectricDist\ELECDIST.Transformer',["SHAPE@"],where)
     for row in cursor:
         row[0]=tlm_107502
