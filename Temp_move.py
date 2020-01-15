f6527fb9dbb6b26d0532e289442898ab48639eea
Macchiato0 <50885101+Macchiato0@users.noreply.github.com>
2020-01-15 14:34:23 -0500

    Update Temp_move.py

--- Temp_move.py
+++ Temp_move.py
@@ -5,14 +5,14 @@ def temp_move(a):#sp move
     edit.startOperation()
     
     where="DEVICELOCATION={}".format(a)
-    cursor=arcpy.da.SearchCursor(r'E:\Data\yfan\sand_box.gdb\sp107502',["SHAPE@"],where)
+    cursor=arcpy.da.SearchCursor(r'E:\Data\yfan\sand_box.gdb\sp032002',["SHAPE@"],where)
     for row in cursor:
-        sp_107502=row[0]        
+        sp_geo=row[0]        
         
     where="DEVICELOCATION={}".format(a)
     cursor=arcpy.da.UpdateCursor(r'E:\Data\yfan\Connection to dgsep011.sde\ELECDIST.ElectricDist\ELECDIST.ServicePoint',["SHAPE@"],where)
     for row in cursor:
-        row[0]=sp_107502
+        row[0]=sp_geo
         cursor.updateRow(row)
         
     edit.stopOperation()
