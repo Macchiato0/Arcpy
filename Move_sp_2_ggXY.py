10e30564d7fa345604bf423b1306e08ea949fa02
Macchiato0 <50885101+Macchiato0@users.noreply.github.com>
2020-03-05 14:31:15 -0500

    Update Move_sp_2_ggXY.py

--- Move_sp_2_ggXY.py
+++ Move_sp_2_ggXY.py
@@ -1,32 +1,35 @@
 #import to E drive
-
+#
 # -*- coding: utf-8 -*-
 # ---------------------------------------------------------------------------
-# import_boundary.py
+# import Move_sp_2_ggXY.py
 # Created on: 2020-03-05 
-#   (Author by Yi Fan)
+# Author by Yi Fan
 # Description: 
+# X is long Y is lat
 # ---------------------------------------------------------------------------
 
 # Import arcpy module
+
 import arcpy
+
 gg_file="XY-address-Lat-Long"
 
 cursor=arcpy.da.SearchCursor(gg_file,["OID_","Lat","Long"])
 
 ggxy={row[0]:(row[1],row[2]) for row in cursor}
 
-def move_p2gg(oid,ggxy):
+def move_p2gg(oid,shp):
     workspace = r'E:\Data\yfan\Connection to dgsep011.sde'
     edit = arcpy.da.Editor(workspace)
     edit.startEditing(False, True)
     edit.startOperation()
-    
-    for row in cursor:
-        pt=row[0]
-    where="OBJECTID={}".format(a)
+    xy=shp[oid]
+    point = arcpy.Point(xy[1]-5,xy[0]-5)
+    ptGeometry = arcpy.PointGeometry(point)
+    where="OBJECTID={}".format(oid)
     cursor=arcpy.da.UpdateCursor(r'E:\Data\yfan\Connection to dgsep011.sde\ELECDIST.ElectricDist\ELECDIST.ServicePoint',["SHAPE@"],where)
     for row in cursor:
-        row[0]=pt
+        row[0]=ptGeometry
         cursor.updateRow(row)
     edit.stopOperation()
