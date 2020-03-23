750aee01f86703a3ebc2272f80f66c7615989107
Macchiato0 <50885101+Macchiato0@users.noreply.github.com>
2020-03-23 09:19:25 -0400

    Update create_16_ciruits_selection.py

--- create_16_ciruits_selection.py
+++ create_16_ciruits_selection.py
@@ -1,14 +1,15 @@
 import json
 with open(r'E:\Data\yfan\sec_cir_update.json', 'rb') as fp:
     data_dict = json.load(fp)
-
+'''
+cursor template
 where="SIXTEENTHSECTIONNAME like '{}%'".format(n)
 cursor=arcpy.da.SearchCursor(r"CE Landbase\Sixteenth Section",["SHAPE@","SIXTEENTHSECTIONNAME"],where)
 
 
 where="FEEDERID= '{}'".format(n)
 cursor=arcpy.da.SearchCursor(r'Org Bounds\Circuit Boundaries',["FEEDERID","SHAPE@"],where)
-
+'''
 
 sixteen_feed={}
 for k in data_dict:
@@ -19,28 +20,38 @@ for k in data_dict:
         for r1 in cursor1:
             sixteen_feed[r1]=fd1
 
+circuit={}
+cursor_circuit=arcpy.da.SearchCursor(r'Org Bounds\Circuit Boundaries',["SHAPE@","FEEDERID"])
+for row in cursor_circuit:
+    fd=row[1]
+    shp=row[0]
+    circuit[fd]=shp
 
+sixteen_feed_0=[]
 for k in data_dict:
     if len(data_dict[k])>1:
-        fd2=data_dict[k]
-        fd2_poly=[]
-        for fd in fd2:
-            where_circuit="FEEDERID= '{}'".format(fd)
-            cursor_circuit=arcpy.da.SearchCursor(r'Org Bounds\Circuit Boundaries',["SHAPE@"],where_circuit)
-            for row_c in cursor_circuit:
-                fd2_poly.append(row_c[0])
-            
-        where2="SIXTEENTHSECTIONNAME like '{}%'".format(k) 
-        cursor2=arcpy.da.SearchCursor(r"CE Landbase\Sixteenth Section",["SIXTEENTHSECTIONNAME","SHAPE@"],where2)
-        for r2 in cursor2:
+        feederid=data_dict[k]
+        feederid_set=set(feederid)
+        fd2=list(feederid_set)
 
-            16_name=r2[0]
-            16_poly=r2[1]
-            sixteen_feed[16_name]=[]
-            for n in range(len(fd2)):
-                if 16_poly.overlaps(fd2_poly[n]) or fd2_poly[n].contains(16_poly):
-                    sixteen_feed[16_name].append(fd2[n])
-                else:
-                    sixteen_feed[16_name]=fd2
+        fd2_poly=[]
+        for f in fd2:
+            fd2_poly.append(circuit[f])    
+        nm=len(fd2)
+        #print nm
+        where2="SIXTEENTHSECTIONNAME like '{}%'".format(k) 
+        cursor2=arcpy.da.SearchCursor(r"CE Landbase\Sixteenth Section",["SIXTEENTHSECTIONNAME","SHAPE@"],where2)        
+        for r2 in cursor2:
+            name_16=r2[0]
+            
+            poly_16=r2[1]
+            #print name_16,poly_16
+            sixteen_feed[name_16]=[]        
+            for n in range(nm):               
+                if poly_16.overlaps(fd2_poly[n]) or fd2_poly[n].contains(poly_16):
+                    sixteen_feed[name_16].append(fd2[n])
+                    #print 't'
+            if len(sixteen_feed[name_16])==0:
+                sixteen_feed_0.append(name_16)
 
 #manually update the tiny circuit
